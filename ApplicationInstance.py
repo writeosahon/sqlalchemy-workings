8a1a0796c2090ddc3b5a581f55c315ba4db3bf97
writeosahon <wrwiteosahon@yahoo.co.uk>
2020-01-27 13:51:53 +0100

    commit

--- ApplicationInstance.py
+++ ApplicationInstance.py
@@ -11,3 +11,9 @@ app = Flask(__name__) # create Flask app instance
 @app.route('/')
 def index(): # define index route
     return "HELLO WORLD FROM ME TO YOU!!"
+
+
+
+# START THE SERVER PROGRAMMATICALLY
+if __name__ == "__main__": # if this module is being executed as the "main" module
+    app.run(debug=True) # run the server (in debug mode)
